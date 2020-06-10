101b8c2232afb36eb07aa0a6aa799ca75736b466
c1nq0 <52049556+c1nq0@users.noreply.github.com>
2020-06-10 08:30:14 +0200

    Fix GetArg int64_t

--- src/util.cpp
+++ src/util.cpp
@@ -603,7 +603,7 @@ std::string GetArg(const std::string& strArg, const std::string& strDefault)
 }
 
 //if string strArg exists in mapArgs, return the corresponding value (int64), otherwise return strDefault (string) (int64)
-int64 GetArg(const std::string& strArg, int64 nDefault)
+int64_t GetArg(const std::string& strArg, int64_t nDefault)
 {
     if (mapArgs.count(strArg))
         return atoi64(mapArgs[strArg]);
